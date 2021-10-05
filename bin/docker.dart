e5208d6210f3be19ed87cc1391aeb7abbda388e1
Maximilian Fischer <github@maaeps.de>
2021-10-05 19:48:08 +0200

    Update docker.dart

--- bin/docker.dart
+++ bin/docker.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'geologger.dart' as geologger;
 
 main() {
-  geologger
-      .main('run -f /var/log/access.log -d /app/GeoLite2-City.mmdb'.split(' '));
+  var envVars = Platform.environment;
+  geologger.main(
+      'run -f ${envVars['LOG_FILE'] ?? '/var/log/access.log'} -d ${envVars['DATABASE_FILE'] ?? '/app/GeoLite2-City.mmdb'}'
+          .split(' '));
 }
